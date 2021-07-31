9953d0717c3a9bf27c27246e2458761520f6fb5e
andres7mo <fernandosito2010@gmail.com>
2021-07-31 15:18:18 -0500

    STORAGE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@capacitor/storage';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  usuarios: any= [];
+  constructor( ) {}
+  ngOnInit(): void {
+    Storage.set({key: 'usuarios', value: JSON.stringify([])});
+  }
 }
---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@capacitor/storage';
+@Component({
+  selector: 'app-tab4',
+  templateUrl: './tab4.page.html',
+  styleUrls: ['./tab4.page.scss'],
+})
+export class Tab4Page implements OnInit {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  Usuario = {
+
+    nombre: '',
+    apellido: '',
+    telefono: '',
+    correo: '',
+  };
+  mostrarusuario: any;
+  constructor() { }
+  ngOnInit() {
+  }
+  async guardardato(){
+    const {value}: any= await Storage.get({key: 'usuarios'});
+    console.log(value.value);
+    const listausuario= JSON.parse(value);
+    listausuario.push(this.Usuario);
+    Storage.set({key: 'usuarios', value: JSON.stringify(listausuario)});
+    this.recuperar();
+  }
+
+  async recuperar(){
+    const {value}: any= await Storage.get({key: 'usuarios'});
+    console.log(value.value);
+     this.mostrarusuario= JSON.parse(value);
+  }
+}
